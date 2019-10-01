76caef6e5ec311a27b5392f8875fe57345a83134
Alex Dyakov <glmrvn@gmail.com>
2019-10-01 21:31:25 +0300

    Added validating for frames

--- code.js
+++ code.js
@@ -1,1 +1,15 @@
+//Validate regex. Only lowercase (a-z ) and with '_' frames 
+var regex = /^[a-z\d_,]*$/;
+
+//Search all frames on the current page
+const allNodes = figma.currentPage.findAll(node => node.type === "FRAME").map(it => it.name).toString();
+
+//Showing alert
+if (regex.test(allNodes) == true) {
+    alert('Cool 😎');
+}
+else {
+    alert('🚨🚨🚨 Сheck your frame names');
+}
+
 figma.closePlugin()
