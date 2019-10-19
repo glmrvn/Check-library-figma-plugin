fca3223a5b6b6c8e4cf32feb95294eb1a0bfc182
glmrvn <glmrvn@gmail.com>
2019-10-19 09:02:04 +0300

    Fixed code comments

--- code.js
+++ code.js
@@ -1,17 +1,17 @@
-//Validate regex. Only lowercase (a-z) and with '_' frames 
+//Validate regex. Only lowercase (a-z) with '_'
 var regex = /^[a-z\d_]*$/;
 var allNodes;
 var problemObjects = [];
 let count = 0;
 
-//Search all frames and instances on the current page
+//Search frames and instances on the current page
 const allFrames = figma.currentPage.findAll(node => node.type === "FRAME" && node.parent.type != "FRAME");
 const allInstances = figma.currentPage.findAll(node => node.type === "INSTANCE" && node.parent.type != "INSTANCE" && node.parent.type != "FRAME");
 
-//Merging frame and instances names
+//Merging frame and instances
 allNodes = allFrames.concat(allInstances);
 
-// Check frames and instances with regex
+// Validating frames and instances with regex
 for (let index in allNodes) {
     let frame = allNodes[index];
     if (regex.test(frame.name) != true) {
@@ -21,16 +21,16 @@ for (let index in allNodes) {
     }
 }
 
-// Showing alert
+// Showing notification
 if (count == 0) {
     figma.notify('Cool 😎', { timeout: 1500 });
 }
 else {
-    // Selecting problem elements
+    // Selecting problem elements and move to viewport
     figma.currentPage.selection = problemObjects;
     figma.viewport.scrollAndZoomIntoView(problemObjects);
 
-    // Error alert text
+    // Error notification text
     figma.notify('🚨🚨🚨 You are have ' + count + ' errors', { timeout: 3000 });
 }
 
