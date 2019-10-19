178034849bf22f0efb7668e98f2e95a4d31686ed
glmrvn <glmrvn@gmail.com>
2019-10-19 08:49:41 +0300

    Added moving to proble objects

--- code.js
+++ code.js
@@ -1,12 +1,12 @@
-//Validate regex. Only lowercase (a-z ) and with '_' frames 
-var regex = /^[a-z\d_,]*$/;
+//Validate regex. Only lowercase (a-z) and with '_' frames 
+var regex = /^[a-z\d_]*$/;
 var allNodes;
 var problemObjects = [];
 let count = 0;
 
 //Search all frames and instances on the current page
-const allFrames = figma.currentPage.findAll(node => node.type === "FRAME" && node.parent.type != "FRAME")
-const allInstances = figma.currentPage.findAll(node => node.type === "INSTANCE" && node.parent.type != "INSTANCE" && node.parent.type != "FRAME")
+const allFrames = figma.currentPage.findAll(node => node.type === "FRAME" && node.parent.type != "FRAME");
+const allInstances = figma.currentPage.findAll(node => node.type === "INSTANCE" && node.parent.type != "INSTANCE" && node.parent.type != "FRAME");
 
 //Merging frame and instances names
 allNodes = allFrames.concat(allInstances);
@@ -15,7 +15,7 @@ allNodes = allFrames.concat(allInstances);
 for (let index in allNodes) {
     let frame = allNodes[index];
     if (regex.test(frame.name) != true) {
-        count++
+        count++;
         problemObjects.push(frame);
         continue;
     }
@@ -26,11 +26,13 @@ if (count == 0) {
     alert('Cool 😎');
 }
 else {
+    // Selecting problem elements
+    figma.currentPage.selection = problemObjects;
+    figma.viewport.scrollAndZoomIntoView(problemObjects);
+
+    // Error alert text
     alert('🚨🚨🚨 You have ' + count + ' errors.');
 }
-
-// Selecting problem elements
-figma.currentPage.selection = problemObjects;
 
 // Close plugin
 figma.closePlugin();
